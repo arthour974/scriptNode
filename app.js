343c8d4d7cae1467206c2a8a73dbfb2842a821b4
Arthur odon <arthurodon6@gmail.com>
2021-08-23 08:39:32 +0200

    static ip

--- app.js
+++ app.js
@@ -18,53 +18,53 @@ var server = http.createServer(function(req, res) {
 
 fixieUrl("https://sheltered-lake-92137.herokuapp.com/", (err, res, body)=>{
   console.log("got reponse: "+ res.statusCode)
+  cron.schedule('*/5 * * * *', async function() {
+      const con = await mysql.createPool({
+        host: process.env.DB_HOST_DOLIBARR,
+        user: process.env.DB_USER_DOLIBARR,
+        port: process.env.DB_DATABASE_PORT,
+        password: process.env.DB_PASSWORD_DOLIBARR,
+        database: process.env.DB_DATABASE_DOLIABRR
+      });
+      
+      await con.getConnection(async function (err) {
+        if (err) {
+          console.log("[mysql error]",err);
+        } else {
+          console.log("Connecté à la base de données MySQL!");
+          await con.query("SELECT * FROM alpha_pneu", async function (err, rows) {
+            if (err) {
+              throw err
+            } else {
+              const client = new ftp.Client();
+              client.ftp.verbose = true;
+              const csv = new ObjectsToCsv(rows)
+              await csv.toDisk('./data/data.csv')
+              try {
+                await client.access({
+                  host: process.env.DB_HOST_FTP,
+                  port: process.env.DB_PORT_FTP,
+                  user: process.env.DB_USER_FTP,
+                  password: process.env.DB_PASSWORD_FTP,
+                })
+                console.log(await client.list());
+                await client.uploadFromDir("./data/", "./")
+              } catch (erro) {
+                console.log(err)
+              }
+               client.close()
+               await con.end(function(err){
+                 if(err){
+                   return console.log(err.message)
+                 } else {
+                   console.log("connection terminé");
+                 }
+               })
+            }
+          })
+        }
+      });
+  });
 })
 
-cron.schedule('*/5 * * * *', async function() {
-    const con = await mysql.createPool({
-      host: process.env.DB_HOST_DOLIBARR,
-      user: process.env.DB_USER_DOLIBARR,
-      port: process.env.DB_DATABASE_PORT,
-      password: process.env.DB_PASSWORD_DOLIBARR,
-      database: process.env.DB_DATABASE_DOLIABRR
-    });
-    
-    await con.getConnection(async function (err) {
-      if (err) {
-        console.log("[mysql error]",err);
-      } else {
-        console.log("Connecté à la base de données MySQL!");
-        await con.query("SELECT * FROM alpha_pneu", async function (err, rows) {
-          if (err) {
-            throw err
-          } else {
-            const client = new ftp.Client();
-            client.ftp.verbose = true;
-            const csv = new ObjectsToCsv(rows)
-            await csv.toDisk('./data/data.csv')
-            try {
-              await client.access({
-                host: process.env.DB_HOST_FTP,
-                port: process.env.DB_PORT_FTP,
-                user: process.env.DB_USER_FTP,
-                password: process.env.DB_PASSWORD_FTP,
-              })
-              console.log(await client.list());
-              await client.uploadFromDir("./data/", "./")
-            } catch (erro) {
-              console.log(err)
-            }
-             client.close()
-             await con.end(function(err){
-               if(err){
-                 return console.log(err.message)
-               } else {
-                 console.log("connection terminé");
-               }
-             })
-          }
-        })
-      }
-    });
-});
 server.listen(process.env.PORT || 5000);
